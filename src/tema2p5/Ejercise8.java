4677919b5cae600fdd93d397d87819482bbbab10
Jose Manuel <Jose Manuel@DESKTOP-L104J9M>
2023-11-13 20:41:47 +0100

    for

--- src/tema2p5/Ejercise8.java
+++ src/tema2p5/Ejercise8.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class Ejercise8 {
 
 	public static void main(String[] args) {
-		int num=0,contador;
+		int num=0,contador=0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Escribe la nota de 5 alumnos: ");
 		for (contador=1;contador<=5;contador++) {
 		num = sc.nextInt();
-		}if (num<5) {
+		System.out.print("");
+		}
+		if (num<5) {
 			System.out.print("hay algun suspenso");
 		}
 	}
