a67167194756a57b4cd11793158d004e6932582a
josem.martin <josem.martin@106-21.info2>
2023-11-14 14:32:55 +0100

    bucles

---
+++ src/t2bucles/Ejercise4.java
@@ -0,0 +1,20 @@
+package t2bucles;
+
+import java.util.Scanner;
+
+public class Ejercise4 {
+
+	public static void main(String[] args) {
+		int numA,numB;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Escribe un numero: ");
+		numA=sc.nextInt();
+		System.out.print("Escribe otro numero: ");
+		numB=sc.nextInt();
+		
+		
+		
+
+	}
+
+}
