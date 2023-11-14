c8e1bb9e61cd21e3a865c95ec86d13de73114930
Jose Manuel <Jose Manuel@DESKTOP-L104J9M>
2023-11-14 20:22:20 +0100

    a`siodfkc

--- src/t2bucles/Ejercise5.java
+++ src/t2bucles/Ejercise5.java
@@ -2,24 +2,24 @@ package t2bucles;
 
 import java.util.Scanner;
 
-public class Ejercise4 {
+public class Ejercise5 {
 
 	public static void main(String[] args) {
-		int numA,numB,mcd=1;
+		int numA,numB, mcm=1;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Escribe un numero: ");
 		numA=sc.nextInt();
 		System.out.print("Escribe otro numero: ");
 		numB=sc.nextInt();
 		if (numA>numB) {
-			for (int i = numB;numA % i == 0 && numB % i == 0; i--) {
-	                mcd = i;
+			for (int i = numA;numA % i == 0 && numB % i == 0; i++) {
+	                mcm = i;
 	            }
 	   }else if(numA<numB){
-		   for (int i = numA;numA % i == 0 && numB % i == 0; i--) {
-               mcd = i;
+		   for (int i = numB;numA % i == 0 && numB % i == 0; i++) {
+               mcm = i;
 		   }
 	   }
-	        System.out.println("El máximo común divisor de " + numA + " y " + numB + " es: " + mcd);
+	        System.out.println("El minimo común multiplo de " + numA + " y " + numB + " es: " + mcm);
 	}
 }
